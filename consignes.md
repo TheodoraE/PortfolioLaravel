7ef1d599549b66e10f344675f3d6d28d60268554
TheodoraE <tidoraa@gmail.com>
2021-02-25 13:08:01 +0100

    template

---
+++ consignes.md
@@ -0,0 +1,28 @@
+# Projet Portfolio Perso
+Trouver un portfolio en bootstrap4 et le laraveliser
+
+## Technologie qu'il faut utiliser :
+Bootstrap4/Laravel
+Git/GitHub
+
+## Pages(sections) minimum requises :
+- A propos( about me.. )
+- Création
+- Competence
+- Contact
+
+## Minimum requis : 
+Template(extends, yield,include etc)
+Crud complet pour chaque section
+La db doit etre créer avec des fichiers migrations
+Des seeders ou factory selon besoin pour chaques données
+Les images doivent déjà être dans public
+Des validates
+Des old
+Des routes nomées
+
+Have fun && Good Luck
+
+Je dois pouvoir prendre votre projet faire un php artisan migrate --seed et que tout saffiche correctement
+
+Deadlines Vendredi 14h30
---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Portfolio</title>
+
+    <!-- Favicons -->
+    <link href="{{asset('img/favicon.png')}}" rel="icon">
+    <link href="{{asset('img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/icofont/icofont.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/venobox/venobox.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+</head>
+<body>
+    
+    {{-- Header --}}
+
+
+    {{-- Content --}}
+    @yield('content')
+
+    {{-- Credits --}}
+    <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/personal-free-resume-bootstrap-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+    </div>
+    
+      <!-- Vendor JS Files -->
+      <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+      <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+      <script src="{{asset('vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+      <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+      <script src="{{asset('vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+      <script src="{{asset('vendor/counterup/counterup.min.js')}}"></script>
+      <script src="{{asset('vendor/owl.carousel/owl.carousel.min.js')}}"></script>
+      <script src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+      <script src="{{asset('vendor/venobox/venobox.min.js')}}"></script>
+    
+      <!-- Template Main JS File -->
+      <script src="{{asset('js/main.js')}}"></script>
+</body>
+</html>
